78f6907d0362a06815357534a63cdb674416734c
pavithra <gnpavithra2017@gmail.com>
2018-06-12 15:19:32 +0530

    commit second time
    commit second time

--- src/main/java/com/mgl/WelcomeMgl.java
+++ src/main/java/com/mgl/WelcomeMgl.java
@@ -5,6 +5,7 @@ public class WelcomeMgl {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Secong prj");
+		System.out.println("Editing Second time");
 	}
 
 }
